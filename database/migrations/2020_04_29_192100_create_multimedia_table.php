83166a61724f496cbe50044f8fc0cd7025816f41
ISMAEL GARCIA <fging2019@gmail.com>
2020-04-29 15:59:42 -0430

    Creacion_migraciones

---
+++ database/migrations/2020_04_29_192100_create_multimedia_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMultimediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('multimedia', function (Blueprint $table) {
+            $table->id('id_multimedia');
+            $table->string('file_path');
+            $table->string('titulo_multimedia');
+            $table->string('descripcion');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('multimedia');
+    }
+}
